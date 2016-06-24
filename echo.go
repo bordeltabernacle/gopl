00cf56b84a4b3bf87464501adfeb9791a1832890
Rob Phoenix <robertphoenix@gmail.com>
2016-06-24 20:15:58 +0100

    move echo program to new file & modify

---
+++ echo.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	command := os.Args[0]
+	fmt.Println("Command: " + command)
+	for i, arg := range os.Args[1:] {
+		fmt.Println(strconv.Itoa(i+1) + ": " + arg)
+	}
+}
