ca74aadfba2ef944af8fba7bc05cf5e9d6724e7c
Rob Phoenix <robertphoenix@gmail.com>
2016-06-26 21:16:19 +0100

    modify dup to filenames where diplicates occur

--- dup.go
+++ dup.go
@@ -7,24 +7,31 @@ import (
 )
 
 func main() {
-	counts := make(map[string]int)
+	dupFiles := make(map[string]map[string]int)
 	files := os.Args[1:]
 	if len(files) == 0 {
+		counts := make(map[string]int)
 		countLines(os.Stdin, counts)
 	} else {
-		for _, arg := range files {
+		for i, arg := range files {
+			counts := make(map[string]int)
+			filename := files[i]
 			f, err := os.Open(arg)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "dup: %v\n", err)
 				continue
 			}
 			countLines(f, counts)
+			dupFiles[filename] = counts
 			f.Close()
 		}
 	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+	for filename, counts := range dupFiles {
+		fmt.Println("File: ", filename)
+		for line, n := range counts {
+			if n > 1 {
+				fmt.Printf("%d\t%s\n", n, line)
+			}
 		}
 	}
 }
